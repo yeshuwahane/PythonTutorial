5649ce7a9a7f93700d3c49510a104beebbf8c485
yeshuwahane <yeshuwahane@gmail.com>
2019-12-10 12:26:13 -0500

    String

---
+++ 4string.py
@@ -0,0 +1,9 @@
+var = "i am God ,  LIght"
+print(len(var))
+print(var.endswith("light"))
+print(var.count("I"))
+print(var.capitalize())
+print(var.find("am"))
+print(var.lower())
+print(var.upper())
+print(var.replace("am","was"))
