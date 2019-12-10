49ea73ed3ca30f36a2fe50384bd6805fbc760293
yeshuwahane <yeshuwahane@gmail.com>
2019-12-10 12:29:11 -0500

    if else

---
+++ 10if_else.py
@@ -0,0 +1,15 @@
+print("Enter a number  ")
+var = int (input())
+print("Enter second number : ")
+varw = int (input())
+
+if var>varw:
+    print(var,"is greater")
+elif var==varw:
+    print("Both are the same value")
+else:
+    print(varw,"is greater")
+
+varq = ["yeshu","alien","light","ray"]
+if "Alien" in varq:
+    print("yes it is ")
